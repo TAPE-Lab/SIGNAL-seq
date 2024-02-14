9c8233abfd5168fde3e1da22597b5720f551f6ac
JamesOpz <j.opzoomer@ucl.ac.uk>
2024-02-14 17:02:13 +0000

    rna mod

--- figure_2_pdo/00_preprocessing_rna/00_run_zumis.sh
+++ figure_2_pdo/00_preprocessing_rna/00_run_zumis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
 # Run the zUMIs pipeline
-# Each sublibrary was run via an independant .yaml file
+# Each sublibrary was run via an independent .yaml file
 # -c specifies the use of a conda environment
 ~/zUMIs/zUMIs.sh -c -y ~/PATH/ex0015_rna_76.yaml
